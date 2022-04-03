37354bcc1a65c7f9f04a5b8b4d232c7c17600230
q-bit <t.quante@outlook.com>
2022-04-03 13:26:32 +0200

    feat: add placeholder if no partner is selected

--- frontend/src/components/chat/AppChatHistoryList.vue
+++ frontend/src/components/chat/AppChatHistoryList.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
-import { ref, watch, computed, nextTick, ComputedRef } from 'vue';
+import { ref, watch, computed, nextTick, inject, ComputedRef, Ref } from 'vue';
 import AppChatHistoryItem from './AppChatHistoryItem.vue';
+import AppContainer from '../layout/AppContainer.vue';
+import iMicrophone from '../icons/iMicrophone.vue';
+import { PublicUser } from '../../store/users';
 import { ChatHistoryItem } from '../../store/chat';
+
+const chatPartner = inject('chatPartner') as Ref<PublicUser>;
 
 const props = defineProps<{ historyItems: ChatHistoryItem[] }>();
 
@@ -17,18 +22,35 @@ watch(historyItemCount, () => {
 
 <template>
   <div class="relative h-full">
-    <ul
-      ref="chatHistoryListElement"
-      class="absolute top-16 left-0 bottom-12 max-h-full w-full overflow-x-hidden overflow-y"
-    >
-      <transition-group name="fade-from-right">
-        <app-chat-history-item
-          v-for="historyItem in historyItems"
-          :key="historyItem.dateSent"
-          :history-item="historyItem"
-        ></app-chat-history-item>
-      </transition-group>
-    </ul>
+    <div class="absolute top-16 left-0 bottom-12 max-h-full w-full">
+      <transition name="fade" mode="out-in">
+        <ul
+          v-if="historyItemCount > 0"
+          ref="chatHistoryListElement"
+          class="max-h-full w-full overflow-x-hidden overflow-y"
+        >
+          <transition-group name="fade-from-right">
+            <app-chat-history-item
+              v-for="historyItem in historyItems"
+              :key="historyItem.dateSent"
+              :history-item="historyItem"
+            ></app-chat-history-item>
+          </transition-group>
+        </ul>
+      </transition>
+
+      <transition name="fade" mode="out-in">
+        <app-container v-if="historyItemCount === 0" tag="div" page center>
+          <h2 class="text-violet-800 dark:text-violet-400 text-2xl mb-4">
+            Start your chat with {{ chatPartner.username }}
+          </h2>
+          <p>
+            Click on the <i-microphone class="inline"></i-microphone> symbol to <br />
+            start recording a message
+          </p>
+        </app-container>
+      </transition>
+    </div>
   </div>
 </template>
 
